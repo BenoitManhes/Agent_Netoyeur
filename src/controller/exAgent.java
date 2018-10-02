637bb0997e8e8fd00e3b1062fbdbcea5dd6f8f93
Lilian <lilianpattier@gmail.com>
2018-10-02 23:58:36 +0200

    explo informe : Glouton fonctionnel
    Prout

--- src/controller/exAgent.java
+++ src/controller/exAgent.java
@@ -12,16 +12,13 @@ public class exAgent implements Runnable{
 
 		/**Initialisation de l agent*/
 		Draw drawing = new Draw(Parametre.TITRE_AGENT, Environement.agent.getListElementObs());
-
-	  
-	   Environement.agent.setX(0);
-	   Environement.agent.setY(0);
-	   testAInformee();
+		
+		//testAInformee();
 	   //testArbre();
 		/**Gestion de l agent*/
 		while(true) {	// gestion de l agent en boucle infini
 
-			/*// Observer environement
+			// Observer environement
 			if(Environement.agent.getObjectifs().isEmpty()) { // si aucun objectif est en attente
 				Environement.agent.observerEnvironnement();
 			}
@@ -30,8 +27,9 @@ public class exAgent implements Runnable{
 				// Mise a jour Etat
 				int nbElementObs = Environement.agent.getListElementObs().size();
 				if(nbElementObs>0) {
-					int X = Math.min(nbElementObs,8);
-					planificationItineraire(X);
+					//int X = Math.min(nbElementObs,8);
+					//planificationItineraire(X);
+					planificationInformee();
 				}
 			}
 			// Decision Action 
@@ -45,7 +43,6 @@ public class exAgent implements Runnable{
 
 			// Action
 			actionAgent();
-			*/
 
 			drawing.render();//mis a jour affichage avec drawing
 			try {Thread.sleep(Parametre.DELAI_AGENT);} catch (InterruptedException e) {e.printStackTrace();}
@@ -96,24 +93,27 @@ public class exAgent implements Runnable{
 	}
 	
 	public void testAInformee() {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 8; i++) {
 			int x = (int)(Math.random()*10);
 			int y = (int)(Math.random()*10);
 			model.Environement.agent.getListElementObs().add(new Poussiere(x, y));
 		}
-		Environement.agent.getListElementObs().add(0,new Poussiere(9, 9));
-		System.out.println(Environement.agent.getListElementObs().toString());
 		
-		Astar arbreinf = new Astar(Environement.agent.getX(), Environement.agent.getY(), Environement.agent.getListElementObs(), Environement.agent.getListElementObs().get(0)); 
-		arbreinf.creationGraph();
-		arbreinf.explorationGraph();
-		Environement.agent.setObjectifs(arbreinf.getItineraireOptimal());	
+		Glouton arbreinf = new Glouton(Environement.agent.getX(), Environement.agent.getY(), Environement.agent.getListElementObs(), Environement.agent.getListElementObs().get(0));
+		arbreinf.greedySearch();
+		Environement.agent.setObjectifs(arbreinf.getItineraireOptimal());
 	}
 	
 
 	public void planificationItineraire(int X) {
 		ArbreNonInforme A = new ArbreNonInforme(Environement.agent.getListElementObs(),X, Environement.agent.getX(), Environement.agent.getY());
 		Environement.agent.setObjectifs(A.getItineraireOptimal());
+	}
+	
+	public void planificationInformee() {
+		Glouton arbreinf = new Glouton(Environement.agent.getX(), Environement.agent.getY(), Environement.agent.getListElementObs(), Environement.agent.getListElementObs().get(0));
+		arbreinf.greedySearch();
+		Environement.agent.setObjectifs(arbreinf.getItineraireOptimal());
 	}
 
 	public void planificationAction() {
